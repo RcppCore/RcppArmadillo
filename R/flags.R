3037f5df5c13e1dc0124eb11eedbf30c964440ee
Romain François <romain@r-enthusiasts.com>
2010-02-22 10:23:08 +0000

    try to set the flags

--- R/flags.R
+++ R/flags.R
@@ -16,27 +16,28 @@
 ## You should have received a copy of the GNU General Public License
 ## along with RcppArmadillo.  If not, see <http://www.gnu.org/licenses/>.
 
-.__CxxFlags <- function(Rcpp = TRUE, ...){
+RcppArmadilloCxxFlags <- function(Rcpp = TRUE, ...){
     rcpp <- ifelse(Rcpp, Rcpp:::RcppCxxFlags(...), "")
 
     arma <- system.file("include", package = "RcppArmadillo")
     if (.Platform$OS.type=="windows")
         arma <- paste('"', arma, '"', sep="")
-
-    res <- sprintf('%s -I%s', rcpp, arma)
-}
-
-.__LdFlags <- function(Rcpp = TRUE, ...){
-    rcpp <- ifelse( Rcpp, Rcpp:::LdFlags(...) , "" )
-    arma <- c("-larmadillo" )
-    paste( rcpp, arma, sep = " " )
+    
+    res <- sprintf('%s -I%s -I/usr/include ', rcpp, arma)
 }
 
 CxxFlags <- function(Rcpp = TRUE, ...){
-    cat( .__CxxFlags(Rcpp = Rcpp, ...), sep = " " )
+    cat( RcppArmadilloCxxFlags(Rcpp = Rcpp, ...), sep = " " )
+}
+
+
+RcppArmadilloLdFlags <- function(Rcpp = TRUE, ...){
+    rcpp <- ifelse( Rcpp, Rcpp:::RcppLdFlags(static= !grepl("^linux",R.version$os) ) , "" )
+    arma <- c("-L/usr/lib -larmadillo" )
+    paste( rcpp, arma, "", sep = " " )
 }
 
 LdFlags <- function(Rcpp = TRUE, ...){
-    cat( .__LdFlags(Rcpp, ... ), sep = "" )
+    cat( RcppArmadilloLdFlags(Rcpp, ... ), sep = "" )
 }
 ## no ldflags as user packages would not need to link to RcppArmadillo.so
