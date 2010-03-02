e3f37e016264cabbca694f8fbf9b5d71c9141eaa
Dirk Deelbuettel <edd@debian.org>
2010-03-02 15:46:15 +0000

    added fastLm C++ function with R wrapper, manual page, and unit test

---
+++ R/fastLm.R
@@ -0,0 +1,27 @@
+
+## fastLm.R: Rcpp/Armadillo implementation of lm()
+##
+## Copyright (C)  2010 Dirk Eddelbuettel and Romain Francois
+##
+## This file is part of RcppArmadillo.
+##
+## RcppArmadillo is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+##
+## RcppArmadillo is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with RcppArmadillo.  If not, see <http://www.gnu.org/licenses/>.
+
+fastLm <- function(y, X) {
+
+    stopifnot(is.matrix(X))
+    stopifnot(nrow(y)==nrow(X))
+
+    res <- .Call("fastLm", y, X, package="RcppArmadillo")
+}
--- src/RcppArmadillo.cpp
+++ src/RcppArmadillo.cpp
@@ -1,4 +1,26 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
+//
+// RcppArmadillo.cpp: Rcpp/Armadillo glue
+//
+// Copyright (C)  2010 Dirk Eddelbuettel and Romain Francois
+//
+// This file is part of RcppArmadillo.
+//
+// RcppArmadillo is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// RcppArmadillo is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with RcppArmadillo.  If not, see <http://www.gnu.org/licenses/>.
+
 #include <RcppArmadillo.h>
+
 using namespace Rcpp ;
 using namespace arma ;
 	
@@ -155,4 +177,31 @@ extern "C" SEXP RcppArmadillo_wrap_Op(){
 	res["mat+mat"] = - m1 ;
 	return res ;
 	
+}
+
+extern "C" SEXP fastLm(SEXP sy, SEXP sX) {
+
+    Rcpp::NumericVector yr(sy);
+    Rcpp::NumericMatrix Xr(sX);
+    std::vector<int> dims = Xr.attr("dim") ;
+    int n = dims[0], k = dims[1];
+
+#if ARMA_VERSION_GE_090
+    arma::mat X(Xr.begin(), n, k, false);   // reuses memory and avoids extra copy
+#else
+    arma::mat X(Xr.begin(), n, k);
+#endif
+
+    arma::colvec y(yr.begin(), yr.size());
+
+    arma::colvec coef = solve(X, y);		// fit model y ~ X
+
+    arma::colvec resid = y - X*coef; 
+    double sig2 = trans(resid)*resid/(n-k);
+    arma::mat covmat = sig2 * arma::inv(arma::trans(X)*X);
+	arma::colvec stderrest = sqrt(covmat.diag());
+
+    Rcpp::Pairlist res(Rcpp::Named( "coef", coef),
+                       Rcpp::Named( "stderr", stderrest));
+    return res;
 }
