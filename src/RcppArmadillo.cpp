0a15a5231ffeee7a398ce64bd059e9a3b1a20c3d
Romain François <romain@r-enthusiasts.com>
2010-03-17 12:49:17 +0000

    update RcppArmadillo to use make_list

--- src/RcppArmadillo.cpp
+++ src/RcppArmadillo.cpp
@@ -26,55 +26,27 @@ using namespace arma ;
 	
 extern "C" SEXP RcppArmadillo_wrap(){
 	
-    List output(4); 
-    List matrices(4) ;
-    List rows(2) ;
-    List cols(2) ;
+    // using the _ notation
+    List matrices = make_list( 
+    	_["Mat<int>"]          = eye<imat>( 3,3 ), 
+    	_["Mat<double>"]       = eye<mat>( 3,3 ),
+    	_["Mat<float>"]        = eye<fmat>( 3, 3 ), 
+    	_["Mat<unsigned int>"] = eye<umat>( 3, 3 )
+    	) ;
+    
+    // using the Named(.) = . notation
+    List cols = make_list( 
+    	Named( "Col<double>" ) = zeros<mat>(5,1), 
+    	Named( "Col<float>" )  = zeros<fmat>(5,1)
+    	) ; 
+    
+    // using the Named( . , . ) notation
+    List rows = make_list( 
+    	Named( "Row<double>", zeros<mat>(1,5) ),
+    	Named( "Row<float>" , zeros<fmat>(1,5) )
+    	) ;
 	
-    imat x1 = eye<imat>( 3,3 ) ;
-    // x1.swap_rows(0,1)  ;
-    matrices[0] = x1 ;
-	
-    mat x2 = eye<mat>( 3,3 ) ;
-    // x2.swap_rows(0,1)  ;
-    matrices[1] = x2 ;
-	
-    fmat x3 = eye<fmat>( 3, 3 );
-    // x3.swap_rows(0,1)  ;
-    matrices[2] = x3 ;
-	
-    umat x4 = eye<umat>( 3, 3 );
-    // x4.swap_rows(0,1)  ;
-    matrices[3] = x4 ;
-	
-    colvec r1 = zeros<mat>(5,1);
-    cols[0] = r1  ;
-	
-    fcolvec r2 = zeros<fmat>(5,1);
-    cols[1] = r2  ;
-	
-    rowvec c1 = zeros<mat>(1,5);
-    rows[0] = c1  ;
-	
-    frowvec c2 = zeros<fmat>(1,5);
-    rows[1] = c2  ;
-	
-    std::vector<std::string> names(4) ;
-    names[0] = "Mat<int>" ;
-    names[1] = "Mat<double>" ;
-    names[2] = "Mat<float>" ;
-    names[3] = "Mat<unsigned int>" ;
-    matrices.names() = names ;
-	
-    names.resize(2) ;
-    names[0] = "Row<double>" ;
-    names[1] = "Row<float>" ;
-    rows.names() = names ;
-	
-    names[0] = "Col<double>" ;
-    names[1] = "Col<float>" ;
-    cols.names() = names ;
-	
+    // creating an empty list and grow it on demand
     List fields ;
     field<int> f1( 2, 2 ) ;
     f1( 0, 0 ) = 0 ; 
@@ -95,17 +67,12 @@ extern "C" SEXP RcppArmadillo_wrap(){
     f3(1,1) = zeros<mat>(2,1) ;
     fields["field<colvec>"] = f3 ;
 	
-    output[0] = matrices;
-    output[1] = rows ;
-    output[2] = cols ;
-    output[3] = fields ;
-	
-    names[0] = "matrices : Mat<T>" ; 
-    names[1] = "rows : Row<T>" ;
-    names.push_back( "columns : Col<T>" );
-    names.push_back( "fields  : field<T>" );
-    output.names() = names ;
-	
+    List output = make_list( 
+    	_["matrices : Mat<T>"]  = matrices, 
+    	_["rows : Row<T>"]      = rows,
+    	_["columns : Col<T>"]   = cols, 
+    	_["fields  : field<T>"] = fields ) ;
+    
     return output ;
 	
 }
@@ -113,57 +80,55 @@ extern "C" SEXP RcppArmadillo_wrap(){
 extern "C" SEXP RcppArmadillo_as_Mat(SEXP input_){
 
     List input(input_) ;
-    arma::imat m1 = input[0] ; /* implicit as */
-    arma::mat  m2 = input[1] ; /* implicit as */
-    arma::umat m3 = input[0] ; /* implicit as */
-    arma::fmat m4 = input[1] ; /* implicit as */
+    imat m1 = input[0] ; /* implicit as */
+    mat  m2 = input[1] ; /* implicit as */
+    umat m3 = input[0] ; /* implicit as */
+    fmat m4 = input[1] ; /* implicit as */
 	
-    List res(4) ;
-    res[0] = arma::accu( m1 ) ;
-    res[1] = arma::accu( m2 ) ;
-    res[2] = arma::accu( m3 ) ;
-    res[3] = arma::accu( m4 ) ;
-	
+    List res = make_list( 
+    	accu( m1 ),
+    	accu( m2 ),
+    	accu( m3 ),
+    	accu( m4 ) ) ;
+    
     return res ;
 }
 
 extern "C" SEXP RcppArmadillo_as_Col( SEXP input_){
     List input(input_) ;
-    arma::icolvec m1 = input[0] ; /* implicit as */
-    arma::colvec  m2 = input[1] ; /* implicit as */
-    arma::ucolvec m3 = input[0] ; /* implicit as */
-    arma::fcolvec m4 = input[1] ; /* implicit as */
+    icolvec m1 = input[0] ; /* implicit as */
+    colvec  m2 = input[1] ; /* implicit as */
+    ucolvec m3 = input[0] ; /* implicit as */
+    fcolvec m4 = input[1] ; /* implicit as */
 	
-    List res(4) ;
-    res[0] = arma::accu( m1 ) ;
-    res[1] = arma::accu( m2 ) ;
-    res[2] = arma::accu( m3 ) ;
-    res[3] = arma::accu( m4 ) ;
-	
+    List res = make_list( 
+    	accu( m1 ),
+    	accu( m2 ),
+    	accu( m3 ),
+    	accu( m4 ) ) ;
+    
     return res ;
 }
 
 extern "C" SEXP RcppArmadillo_as_Row(SEXP input_){
     List input(input_) ;
-    arma::irowvec m1 = input[0] ; /* implicit as */
-    arma::rowvec  m2 = input[1] ; /* implicit as */
-    arma::urowvec m3 = input[0] ; /* implicit as */
-    arma::frowvec m4 = input[1] ; /* implicit as */
+    irowvec m1 = input[0] ; /* implicit as */
+    rowvec  m2 = input[1] ; /* implicit as */
+    urowvec m3 = input[0] ; /* implicit as */
+    frowvec m4 = input[1] ; /* implicit as */
 	
-    List res(4) ;
-    res[0] = arma::accu( m1 ) ;
-    res[1] = arma::accu( m2 ) ;
-    res[2] = arma::accu( m3 ) ;
-    res[3] = arma::accu( m4 ) ;
-	
+    List res = make_list( 
+    	accu( m1 ),
+    	accu( m2 ),
+    	accu( m3 ),
+    	accu( m4 ) ) ;
     return res ;
-	
 }
 
 extern "C" SEXP RcppArmadillo_wrap_Glue(){
 	
-    arma::mat m1 = eye<mat>( 3, 3 ) ;
-    arma::mat m2 = eye<mat>( 3, 3 ) ;
+    mat m1 = eye<mat>( 3, 3 ) ;
+    mat m2 = eye<mat>( 3, 3 ) ;
 	
     List res ;
     res["mat+mat"] = m1 + m2 ;
@@ -172,10 +137,10 @@ extern "C" SEXP RcppArmadillo_wrap_Glue(){
 
 extern "C" SEXP RcppArmadillo_wrap_Op(){
 
-    arma::mat m1 = eye<mat>( 3, 3 ) ;
+    mat m1 = eye<mat>( 3, 3 ) ;
 	
     List res ;
-    res["mat+mat"] = - m1 ;
+    res["- mat"] = - m1 ;
     return res ;
 }
 
