41b7277fb2237057c459391bccb8b231111902a4
Dirk Eddelbuettel <edd@debian.org>
2010-05-14 00:58:17 +0000

    RcppArmadillo gets and expandedn fastLm formula interface and summary/print/preditc methods

--- R/fastLm.R
+++ R/fastLm.R
@@ -18,7 +18,7 @@
 ## You should have received a copy of the GNU General Public License
 ## along with RcppArmadillo.  If not, see <http://www.gnu.org/licenses/>.
 
-fastLm <- function(y, X) {
+fastLmCall <- function(y, X) {
 
     stopifnot(is.matrix(X))
     stopifnot(nrow(y)==nrow(X))
@@ -26,18 +26,82 @@ fastLm <- function(y, X) {
     res <- .Call("fastLm", y, X, package="RcppArmadillo")
 }
 
-## What would be nice here:
-##
-##   fastLm <- function(x, ...) UseMethod("fastLm")
-##
-##   fastLm.formula <- ...
-##
-##   fastLm.default <- ...
-##
-##   print.fastLm <- function(x, ...)
-##
-##   summary.fastLm <- function(object, ...
-##
-##   print.summary.fastLm <- ...
-##
-## but on the other hand lm.fit() does not do any of this either
+fastLm <- function(x, ...) UseMethod("fastLm")
+
+fastLm.default <- function(x, y, ...) {
+
+    x <- as.matrix(x)
+    y <- as.numeric(y)
+
+    res <- fastLmCall(y, x)
+
+    names(res$coefficients) <- colnames(res$vcov) <- rownames(res$vcov) <- colnames(x)
+
+    res$fitted.values <- as.vector(x %*% res$coefficients)
+    res$residuals <- y - res$fitted.values
+    res$call <- match.call()
+
+    class(res) <- "fastLm"
+    res
+}
+
+print.fastLm <- function(x, ...) {
+    cat("\nCall:\n")
+    print(x$call)
+    cat("\nCoefficients:\n")
+    print(x$coefficients)
+}
+
+summary.fastLm <- function(object, ...) {
+    se <- object$stderr
+    tval <- coef(object)/se
+
+    TAB <- cbind(Estimate = coef(object),
+                 StdErr = se,
+                 t.value = tval,
+                 p.value = 2*pt(-abs(tval), df=object$df))
+
+    # why do I need this here?
+    rownames(TAB) <- names(object$coefficients)
+    colnames(TAB) <- c("Estimate", "StdErr", "t.value", "p.value")
+
+    res <- list(call=object$call,
+                coefficients=TAB)
+
+    class(res) <- "summary.fastLm"
+    res
+}
+
+print.summary.fastLm <- function(x, ...) {
+    cat("\nCall:\n")
+    print(x$call)
+    cat("\n")
+
+    printCoefmat(x$coefficients, P.value=TRUE, has.Pvalue=TRUE)
+}
+
+fastLm.formula <- function(formula, data=list(), ...) {
+    mf <- model.frame(formula=formula, data=data)
+    x <- model.matrix(attr(mf, "terms"), data=mf)
+    y <- model.response(mf)
+
+    res <- fastLm.default(x, y, ...)
+    names(res$coefficients) <- colnames(x) <- colnames(res$vcov) <- rownames(res$vcov) <- colnames(x)
+    res$call <- match.call()
+    res$formula <- formula
+    res
+}
+
+predict.fastLm <- function(object, newdata=NULL, ...) {
+    if (is.null(newdata)) {
+        y <- fitted(object)
+    } else {
+        if (!is.null(object$formula)) {
+            x <- model.matrix(object$formula, newdata)
+        } else {
+            x <- newdata
+        }
+        y <- as.vector(x %*% coef(object))
+    }
+    y
+}
