99475a521bfe939941f6076cca108637103ad845
Romain François <romain@r-enthusiasts.com>
2010-05-28 16:07:41 +0000

    pull Makevars and Makevars.win from skeleton so that we do not have yet another copy

--- R/inline.R
+++ R/inline.R
@@ -15,18 +15,9 @@
 ## You should have received a copy of the GNU General Public License
 ## along with RcppArmadillo.  If not, see <http://www.gnu.org/licenses/>.
 
-Makevars.RcppArmadillo <- '
-PKG_LIBS = $(shell $(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()" ) $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS)
-'
-Makevars.win.RcppArmadillo <- '
-PKG_LIBS = $(shell $(R_HOME)/bin${R_ARCH_BIN}/Rscript.exe -e "Rcpp:::LdFlags()") $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS)
-'
-
 inline_cxx_plugin <- Rcpp:::Rcpp.plugin.maker(
 	include.before = "#include <RcppArmadillo.h>", 
-	LinkingTo = c("Rcpp", "RcppArmadillo"), 
-	Depends = c("Rcpp", "RcppArmadillo"),
-	libs = "$(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS)", 
-	Makevars = Makevars.RcppArmadillo, 
-	Makevars.win = Makevars.win.RcppArmadillo
+	libs           = "$(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS)", 
+	package        = "RcppArmadillo"
 )
+
