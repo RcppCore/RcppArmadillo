d135867b1406d706df92d5a0caa4fe1e08192327
Romain François <romain@r-enthusiasts.com>
2010-05-28 16:09:00 +0000

    need higher inline

--- inst/unitTests/runTests.R
+++ inst/unitTests/runTests.R
@@ -4,8 +4,8 @@ if( ! require( "inline", character.only = TRUE, quietly = TRUE ) ){
 	stop( "The inline package is required to run RcppArmadillo unit tests" )
 }
 
-if( compareVersion( packageDescription( "inline" )[["Version"]], "0.3.4.2" ) < 0 ){
-	stop( "RcppArmadillo unit tests need at least the version 0.3.4.2 of inline" )
+if( compareVersion( packageDescription( "inline" )[["Version"]], "0.3.4.3" ) < 0 ){
+	stop( "RcppArmadillo unit tests need at least the version 0.3.4.3 of inline" )
 }
 
 if(require("RUnit", quietly = TRUE)) {
