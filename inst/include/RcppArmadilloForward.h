80af38e408e716f4e6c1e04efafb24aa52ecd4e3
Romain François <romain@r-enthusiasts.com>
2010-06-02 11:30:52 +0000

    support for wrap( mtOp )

--- inst/include/RcppArmadilloForward.h
+++ inst/include/RcppArmadilloForward.h
@@ -57,7 +57,10 @@ namespace Rcpp {
     
     template <typename T1, typename T2, typename glue_type>
     SEXP wrap(const arma::eGlueCube<T1,T2,glue_type>& X ) ;
-    
+
+    template<typename out_eT, typename T1, typename op_type>
+    SEXP wrap( const arma::mtOp<out_eT,T1,op_type>& X ) ;
+
     namespace traits {
 
 	/* support for as */
