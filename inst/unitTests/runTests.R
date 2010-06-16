1a263869cb7f2fea33a6fa603077b50d3fe14249
Romain François <romain@r-enthusiasts.com>
2010-06-16 12:51:18 +0000

    keeping RUnit results in the RcppArmadillo.Rcheck directory on R CMD check

--- inst/unitTests/runTests.R
+++ inst/unitTests/runTests.R
@@ -49,15 +49,21 @@ if(require("RUnit", quietly = TRUE)) {
             }
         }
 
-        # give a chance to the user to customize where he/she wants
-        # the unit tests results to be stored with the --output= command
-        # line argument
-        if( exists( "argv",  globalenv() ) ){
-            ## littler
-            output <- process_args(argv)
-        } else {
-            ## Rscript
-            output <- process_args(commandArgs(TRUE))
+        # R CMD check uses this
+        if( exists( "RcppArmadillo.unit.test.output.dir", globalenv() ) ){
+			output <- RcppArmadillo.unit.test.output.dir
+		} else {
+
+			# give a chance to the user to customize where he/she wants
+        	# the unit tests results to be stored with the --output= command
+        	# line argument
+        	if( exists( "argv",  globalenv() ) ){
+        	    ## littler
+        	    output <- process_args(argv)
+        	} else {
+        	    ## Rscript
+        	    output <- process_args(commandArgs(TRUE))
+        	} 
         }
 
         # if it did not work, try to use /tmp
