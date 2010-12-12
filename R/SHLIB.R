3d461f68ad460ac6e75a0d63b6058e43aef983b7
Romain François <romain@r-enthusiasts.com>
2010-12-12 12:46:20 +0000

    also RcppArmadillo:::SHLIB

---
+++ R/SHLIB.R
@@ -0,0 +1,23 @@
+## Copyright (C)       2010 Dirk Eddelbuettel, Romain Francois and Douglas Bates
+##
+## This file is part of RcppArmadillo.
+##
+## RcppArmadillo is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+##
+## RcppArmadillo is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with RcppArmadillo.  If not, see <http://www.gnu.org/licenses/>.
+
+SHLIB <- Rcpp:::SHLIB.maker( 
+    env = list( 
+        PKG_LIBS = sprintf( "%s $(LAPACK_LIBS) $(BLAS_LIBS) $(FLIBS)", Rcpp:::RcppLdFlags() ),
+        PKG_CPPFLAGS = sprintf( "%s %s", Rcpp:::RcppCxxFlags(), RcppArmadillo:::RcppArmadilloCxxFlags() )
+   )
+)
