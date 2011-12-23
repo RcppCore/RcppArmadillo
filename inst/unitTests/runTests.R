ba545aa403d76473e1c65abdac49c0b9446a1239
Dirk Eddelbuettel <edd@debian.org>
2011-12-23 13:52:38 +0000

    modify fallback location of unitTest output files to be the RcppArmadillo.Rcheck/ dir

--- inst/unitTests/runTests.R
+++ inst/unitTests/runTests.R
@@ -63,16 +63,11 @@ if(require("RUnit", quietly = TRUE)) {
         	} else {
         	    ## Rscript
         	    output <- process_args(commandArgs(TRUE))
-        	} 
+        	}
         }
 
-        # if it did not work, try to use /tmp
-        if( is.null(output) ){
-            if( file.exists( "/tmp" ) ){
-                output <- "/tmp"
-            } else{
-                output <- getwd()
-            }
+        if( is.null(output) ) {         # if it did not work, use parent dir
+            output <- ".."              # as BDR does not want /tmp to be used
         }
 
         ## Print results
