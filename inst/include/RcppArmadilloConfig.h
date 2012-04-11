f135148008a7b1d5742be02910db8b0242bbbbc1
Dirk Eddelbuettel <edd@debian.org>
2012-04-11 20:31:04 +0000

    protect #define of ARMA_DEFAULT_OSTREAM by #ifndef test

--- inst/include/RcppArmadilloConfig.h
+++ inst/include/RcppArmadilloConfig.h
@@ -61,7 +61,9 @@
 
 // Rcpp has its own stream object which cooperates more nicely with R's i/o
 // And as of Armadillo 2.4.3, we can use this stream object as well 
+#if !defined(ARMA_DEFAULT_OSTREAM)
 #define ARMA_DEFAULT_OSTREAM Rcpp::Rcout
+#endif
 
 #endif
 
