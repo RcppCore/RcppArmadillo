992dd882ce30e1f3a59f39c98434a70eb84e7a8f
Dirk Eddelbuettel <edd@debian.org>
2012-07-19 00:31:59 +0000

    o improved test for intercept once more with a tip of the hat to Doug

--- R/fastLm.R
+++ R/fastLm.R
@@ -42,7 +42,7 @@ fastLm.default <- function(X, y, ...) {
     res$fitted.values <- as.vector(X %*% res$coefficients)
     res$residuals <- y - res$fitted.values
     res$call <- match.call()
-    res$intercept <- any(apply(X, 2, function(x) all(x == 1)))
+    res$intercept <- any(apply(X, 2, function(x) all(x == x[1])))
 
     class(res) <- "fastLm"
     res
