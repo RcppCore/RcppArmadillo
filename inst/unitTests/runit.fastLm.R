bfafbbeec8f5c3db2bb894ac11659e3e538d7a61
Dirk Deelbuettel <edd@debian.org>
2012-07-21 20:48:53 +0000

    o expanded unit tests for fastLm  o simplified two stopifnot calls into one, and added a third (a la RcppEigen)  o rolled minor revision by one to indicate that this is not a CRAN release version

--- inst/unitTests/runit.fastLm.R
+++ inst/unitTests/runit.fastLm.R
@@ -1,6 +1,6 @@
 #!/usr/bin/r -t
 #
-# Copyright (C) 2010 - 2011  Dirk Eddelbuettel, Romain Francois and Douglas Bates
+# Copyright (C) 2010 - 2012  Dirk Eddelbuettel, Romain Francois and Douglas Bates
 #
 # This file is part of RcppArmadillo.
 #
@@ -34,6 +34,64 @@ test.fastLm <- function() {
                 msg="fastLm.coef")
     checkEquals(as.numeric(flm$stderr), as.numeric(coef(summary(fit))[,2]),
                 msg="fastLm.stderr")
+    checkEquals(as.numeric(flm$df.residual), as.numeric(fit$df.residual),
+                msg="fastLm.df.residual")
+}
+
+test.fastLm.default <- function() {
+    data(trees, package="datasets")
+    flm <- RcppArmadillo:::fastLm.default(cbind(1, log(trees$Girth)), log(trees$Volume))
+    fit <- lm(log(Volume) ~ log(Girth), data=trees)
+
+    checkEquals(as.numeric(flm$coefficients), as.numeric(coef(fit)),
+                msg="fastLm.default.coef")
+    checkEquals(as.numeric(flm$stderr), as.numeric(coef(summary(fit))[,2]),
+                msg="fastLm.default.stderr")
+    checkEquals(as.numeric(flm$df.residual), as.numeric(fit$df.residual),
+                msg="fastLm.default.df.residual")
+    checkEquals(as.numeric(flm$residuals), as.numeric(fit$residuals),
+                msg="fastLm.default.residuals")
+    checkEquals(as.numeric(flm$fitted.values), as.numeric(fit$fitted.values),
+                msg="fastLm.default.fitted.values")
+}
+
+test.summary.fastLm <- function() {
+    data(trees, package="datasets")
+    sflm <- summary(fastLm(log(Volume) ~ log(Girth), data=trees))
+    sfit <- summary(lm(log(Volume) ~ log(Girth), data=trees))
+
+    checkEquals(as.numeric(coef(sflm)), as.numeric(coef(sfit)),
+                msg="summary.fastLm.coef")
+    checkEquals(sflm$r.squared, sfit$r.squared,
+                msg="summary.fastLm.r.squared")
+    checkEquals(sflm$adj.r.squared, sfit$adj.r.squared,
+                msg="summary.fastLm.r.squared")
+    checkEquals(sflm$sigma, sfit$sigma,
+                msg="summary.fastLm.sigma")
+
+    ## no intercept case
+    sflm <- summary(fastLm(log(Volume) ~ log(Girth) - 1, data=trees))
+    sfit <- summary(lm(log(Volume) ~ log(Girth) - 1, data=trees))
+    checkEquals(as.numeric(coef(sflm)), as.numeric(coef(sfit)),
+                msg="summary.fastLm.coef.noint")
+    checkEquals(sflm$r.squared, sfit$r.squared,
+                msg="summary.fastLm.r.squared.noint")
+    checkEquals(sflm$adj.r.squared, sfit$adj.r.squared,
+                msg="summary.fastLm.r.squared.noint")
+    checkEquals(sflm$sigma, sfit$sigma,
+                msg="summary.fastLm.sigma.noint")
+
+    ## non-formula use
+    sflm <- summary(fastLm(log(trees$Girth), log(trees$Volume)))
+    sfit <- summary(lm(log(Volume) ~ log(Girth) - 1, data=trees))
+    checkEquals(as.numeric(coef(sflm)), as.numeric(coef(sfit)),
+                msg="summary.fastLm.coef.nonform")
+    checkEquals(sflm$r.squared, sfit$r.squared,
+                msg="summary.fastLm.r.squared.nonform")
+    checkEquals(sflm$adj.r.squared, sfit$adj.r.squared,
+                msg="summary.fastLm.r.squared.nonform")
+    checkEquals(sflm$sigma, sfit$sigma,
+                msg="summary.fastLm.sigma.nonform")
 }
 
 test.fastLm.formula <- function() {
@@ -44,5 +102,11 @@ test.fastLm.formula <- function() {
     checkEquals(flm$coefficients, coef(fit), msg="fastLm.formula.coef")
     checkEquals(as.numeric(flm$stderr), as.numeric(coef(summary(fit))[,2]),
                 msg="fastLm.formula.stderr")
+    checkEquals(as.numeric(flm$df.residual), as.numeric(fit$df.residual),
+                msg="fastLm.formula.df.residual")
+    checkEquals(as.numeric(flm$residuals), as.numeric(fit$residuals),
+                msg="fastLm.formula.residuals")
+    checkEquals(as.numeric(flm$fitted.values), as.numeric(fit$fitted.values),
+                msg="fastLm.formula.fitted.values")
 }
 
