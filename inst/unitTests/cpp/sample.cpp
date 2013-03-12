11b8f6efb0250087bbf04e23b4cde9a8abbc58a7
Dirk Deelbuettel <edd@debian.org>
2013-03-12 02:33:57 +0000

    brown bag fix

--- inst/unitTests/cpp/sample.cpp
+++ inst/unitTests/cpp/sample.cpp
@@ -1,26 +1,28 @@
-#!/usr/bin/r -t
-#
-# Copyright (C) 2012 - 2013  Christian Gunning and Dirk Eddelbuettel
-#
-# This file is part of RcppArmadillo.
-#
-# RcppArmadillo is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
-#
-# RcppArmadillo is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with RcppArmadillo.  If not, see <http://www.gnu.org/licenses/>.
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+//
+// sample.cpp: RcppArmadillo unit test code for sample() function
+//
+// Copyright (C) 2012 - 2013  Christian Gunning and Dirk Eddelbuettel
+//
+// This file is part of RcppArmadillo.
+//
+// RcppArmadillo is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// RcppArmadillo is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with RcppArmadillo.  If not, see <http://www.gnu.org/licenses/>.
 
 // [[Rcpp::depends(RcppArmadillo)]]
 #include <RcppArmadillo.h>
 
-#include <add-ons/sample.h>
+#include <RcppArmadilloExtensions/sample.h>
 
 using namespace Rcpp;
 
