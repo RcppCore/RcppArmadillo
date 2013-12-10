9de2d9ae30573705b2bdb88992f268ddcd41af72
Dirk Eddelbuettel <edd@debian.org>
2013-12-09 20:02:07 -0600

    release 0.3.930.1
    fix for zgesdd unavailability in Rlapack
    updated emacs mode headers for src/*cpp
    updated bib entry

--- src/RcppArmadillo.cpp
+++ src/RcppArmadillo.cpp
@@ -1,4 +1,4 @@
-// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
 //
 // RcppArmadillo.cpp: Rcpp/Armadillo glue
 //
