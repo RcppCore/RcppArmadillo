0543d093dfcf1c5500c1be6e89caabfd3f3c94fd
Dirk Eddelbuettel <edd@debian.org>
2014-08-17 07:50:38 -0500

    new sparse matrix unit tests

---
+++ inst/unitTests/cpp/sparse.cpp
@@ -0,0 +1,50 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+//
+// sparse.cpp: RcppArmadillo unit test code for sparse matrices 
+//
+// Copyright (C) 2014  Dirk Eddelbuettel
+//
+// This file is part of RcppArmadillo.
+//
+// RcppArmadillo is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// RcppArmadillo is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with RcppArmadillo.  If not, see <http://www.gnu.org/licenses/>.
+
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
+
+// [[Rcpp::export]]
+arma::sp_mat asSpMat(SEXP S) {
+    return Rcpp::as<arma::sp_mat>(S);
+}
+
+// [[Rcpp::export]]
+arma::sp_mat sparseAddition(arma::sp_mat SM) {
+    return SM + SM;
+}
+
+// [[Rcpp::export]]
+arma::sp_mat sparseMultiplication(arma::sp_mat SM, int k) {
+    return k * SM;
+}
+
+// [[Rcpp::export]]
+arma::sp_mat fromTriplet(arma::urowvec ri, arma::urowvec ci, arma::colvec values) {
+    arma::umat loc = arma::join_vert(ri, ci);// form 2*N 'locations' matrix
+    arma::sp_mat sp(loc, values);            // create sparse from locations and values
+    return sp;
+}
+
+// [[Rcpp::export]]
+arma::sp_mat sparseTranspose(arma::sp_mat SM) {
+    return SM.t();
+}
---
+++ inst/unitTests/runit.sparse.R
@@ -0,0 +1,62 @@
+#!/usr/bin/r -t
+#
+# Copyright (C) 2014  Dirk Eddelbuettel
+#
+# This file is part of RcppArmadillo.
+#
+# RcppArmadillo is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# RcppArmadillo is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RcppArmadillo.  If not, see <http://www.gnu.org/licenses/>.
+
+.setUp <- RcppArmadillo:::unit_test_setup("sparse.cpp") 
+
+## setting up an example matrix -- using the fact that the as<sp_mat>
+## converter prefers sparse matrix objects create by the Matrix package
+suppressMessages(require(Matrix))
+## cf http://people.sc.fsu.edu/~jburkardt/data/st/st.html
+mtxt <- c("11   0   0  14   0  16",
+          " 0  22   0   0  25  26",
+          " 0   0  33  34   0  36",
+          "41   0  43  44   0  46")
+M <- as.matrix(read.table(textConnection(mtxt)))
+dimnames(M) <- NULL
+SM <- Matrix(M, sparse=TRUE)
+
+
+test.as.sparse <- function() {
+    checkEquals(SM, asSpMat(SM), msg="as<sp_mat>")
+}
+
+test.sparse.addition <- function() {
+    checkEquals(SM + SM, sparseAddition(SM), msg="addSparse")
+}
+
+test.sparse.multiplication <- function() {
+    k <- 3
+    checkEquals(k*SM, sparseMultiplication(SM, k), msg="multSparse")
+}
+
+test.sparse.fromTriplet <- function() {
+    mtxt <- c("0 0 1",
+              "0 2 0",
+              "3 0 0")
+    M <- as.matrix(read.table(textConnection(mtxt)))
+    dimnames(M) <- NULL
+    SM <- Matrix(M, sparse=TRUE)
+
+    spM <- fromTriplet(0:2, 2:0, 1:3)
+    checkEquals(SM, spM, msg="fromTriplet")
+}
+
+test.sparse.transpose <- function() {
+    checkEquals(t(SM), sparseTranspose(SM), msg="transposeSparse")
+}
