7e8406fc4564bfbf9f1f68cb32fa98d3263b9946
Dirk Eddelbuettel <edd@debian.org>
2014-11-19 22:07:42 -0600

    release 0.4.549.2.0 with test release 4.549.2alpha of Armadillo: updated mul_gemm.hpp

--- inst/include/armadillo_bits/arma_version.hpp
+++ inst/include/armadillo_bits/arma_version.hpp
@@ -13,7 +13,7 @@
 
 #define ARMA_VERSION_MAJOR 4
 #define ARMA_VERSION_MINOR 549
-#define ARMA_VERSION_PATCH 1
+#define ARMA_VERSION_PATCH 2
 #define ARMA_VERSION_NAME  "unstable development version -- not for production use"
 
 
