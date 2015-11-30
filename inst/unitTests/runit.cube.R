897572de252b8faf3308ad9f2fba9ecb641e032b
nathan-russell <russell.nr2012@gmail.com>
2015-11-30 14:29:16 -0500

    Enable conversion to arma::Cube<T> with unit tests

---
+++ inst/unitTests/runit.cube.R
@@ -0,0 +1,92 @@
+#!/usr/bin/r -t
+#
+# Copyright (C) 2015 Dirk Eddelbuettel and Nathan Russell
+#
+# This file is part of RcppArmadillo.
+#
+# RcppArmadillo is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# RcppArmadillo is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with RcppArmadillo.  If not, see <http://www.gnu.org/licenses/>.
+
+# 30 November 2015
+
+.setUp <- RcppArmadillo:::unit_test_setup("cube.cpp")
+
+test.cube <- function() {
+  
+  ## test arrays
+  dbl_cube <- array(1.5:27.5, rep(3, 3))
+  int_cube <- array(1L:27L, rep(3, 3))
+  cplx_cube <- array(1.5:27.5 + 2i, rep(3, 3))
+  
+  ## check cube (Cube<double>) and fcube (Cube<float>), no NAs
+  checkEquals(cube_test(dbl_cube), (dbl_cube ** 2), "cube_test no NA")
+  checkEquals(fcube_test(dbl_cube), (dbl_cube ** 2), "fcube_test no NA")
+  
+  ## check icube (Cube<sword>) and ucube (Cube<uword>), no NAs
+  checkEquals(icube_test(int_cube), (int_cube ** 2), "icube_test no NA")
+  checkEquals(ucube_test(int_cube), (int_cube ** 2), "ucube_test no NA")
+  
+  ## check cx_cube (Cube<cx_double>) and cx_fcube (Cube<cx_float>), no NAs
+  checkEquals(cx_cube_test(cplx_cube), (cplx_cube ** 2), "cx_cube_test no NA")
+  checkEquals(cx_fcube_test(cplx_cube), (cplx_cube ** 2), "cx_fcube_test no NA",
+              tolerance = 1.5e-7)
+  
+  
+  ## modify test objects to contain NA value
+  dbl_cube[2, 2, 2] <- NA
+  int_cube[2, 2, 2] <- NA
+  cplx_cube[2, 2, 2] <- NA
+  
+  ## check cube (Cube<double>) and fcube (Cube<float>), with NAs
+  checkEquals(cube_test(dbl_cube), (dbl_cube ** 2), "cube_test with NA")
+  checkEquals(fcube_test(dbl_cube), (dbl_cube ** 2), "fcube_test with NA")
+  
+  ## check icube (Cube<sword>) and ucube (Cube<uword>), with NAs
+  checkEquals(icube_test(int_cube), (int_cube ** 2), "icube_test with NA")
+  checkEquals(ucube_test(int_cube), (int_cube ** 2), "ucube_test with NA")
+  
+  ## check cx_cube (Cube<cx_double>) and cx_fcube (Cube<cx_float>), with NAs
+  checkEquals(cx_cube_test(cplx_cube), (cplx_cube ** 2), "cx_cube_test with NA")
+  checkEquals(cx_fcube_test(cplx_cube), (cplx_cube ** 2), "cx_fcube_test with NA",
+              tolerance = 1.5e-7)
+  
+  
+  ## test that exception is thrown with dims(x) != 3
+  dbl_cube <- array(1.5:16.5, rep(2, 4))
+  int_cube <- array(1L:16L, rep(2, 4))
+  cplx_cube <- array(1.5:16.5 + 2i, rep(2, 4))
+  
+  ## cube_test and fcube_test should throw here
+  checkTrue(
+    inherits(try(cube_test(dbl_cube), silent = TRUE), "try-error"),
+    "cube_test bad dimensions")
+  checkTrue(
+    inherits(try(fcube_test(dbl_cube), silent = TRUE), "try-error"),
+    "fcube_test bad dimensions")
+  
+  ## icube_test and ucube_test should throw here
+  checkTrue(
+    inherits(try(icube_test(int_cube), silent = TRUE), "try-error"),
+    "icube_test bad dimensions")
+  checkTrue(
+    inherits(try(ucube_test(int_cube), silent = TRUE), "try-error"),
+    "ucube_test bad dimensions")
+  
+  ## cx_cube_test and cx_fcube_test should throw here
+  checkTrue(
+    inherits(try(cx_cube_test(cplx_cube), silent = TRUE), "try-error"),
+    "cx_cube_test bad dimensions")
+  checkTrue(
+    inherits(try(cx_fcube_test(cplx_cube), silent = TRUE), "try-error"),
+    "cx_fcube_test bad dimensions")
+}
