0c3174384187a2b419b1aa2027d3eecd926ce22e
Dirk Eddelbuettel <edd@debian.org>
2020-06-08 06:42:02 -0500

    armadillo 9.900.1

--- inst/include/armadillo_bits/arma_version.hpp
+++ inst/include/armadillo_bits/arma_version.hpp
@@ -22,7 +22,7 @@
 #define ARMA_VERSION_MAJOR 9
 #define ARMA_VERSION_MINOR 900
 #define ARMA_VERSION_PATCH 1
-#define ARMA_VERSION_NAME  "9.900-RC1"
+#define ARMA_VERSION_NAME  "Nocturnal Misbehaviour"
 
 
 
--- inst/include/armadillo_bits/auxlib_bones.hpp
+++ inst/include/armadillo_bits/auxlib_bones.hpp
@@ -104,27 +104,27 @@ class auxlib
   
   template<typename T1>
   inline static bool eig_gen_balance(Mat< std::complex<typename T1::pod_type> >& vals, Mat< std::complex<typename T1::pod_type> >& vecs, const bool vecs_on, const Base< std::complex<typename T1::pod_type>, T1 >& expr);
-
-
+  
+  
   //
   // eig_gen_twosided
-
+  
   template<typename T1>
   inline static bool eig_gen_twosided(Mat< std::complex<typename T1::pod_type> >& vals, Mat< std::complex<typename T1::pod_type> >& lvecs, Mat< std::complex<typename T1::pod_type> >& rvecs, const Base<typename T1::pod_type,T1>& expr);
-
+  
   template<typename T1>
   inline static bool eig_gen_twosided(Mat< std::complex<typename T1::pod_type> >& vals, Mat< std::complex<typename T1::pod_type> >& lvecs, Mat< std::complex<typename T1::pod_type> >& rvecs, const Base< std::complex<typename T1::pod_type>, T1 >& expr);
-
-
+  
+  
   //
   // eig_gen_twosided_balance
-
+  
   template<typename T1>
   inline static bool eig_gen_twosided_balance(Mat< std::complex<typename T1::pod_type> >& vals, Mat< std::complex<typename T1::pod_type> >& lvecs, Mat< std::complex<typename T1::pod_type> >& rvecs, const Base<typename T1::pod_type,T1>& expr);
-
+  
   template<typename T1>
   inline static bool eig_gen_twosided_balance(Mat< std::complex<typename T1::pod_type> >& vals, Mat< std::complex<typename T1::pod_type> >& lvecs, Mat< std::complex<typename T1::pod_type> >& rvecs, const Base< std::complex<typename T1::pod_type>, T1 >& expr);
-
+  
   
   //
   // eig_pair
@@ -134,17 +134,17 @@ class auxlib
   
   template<typename T1, typename T2>
   inline static bool eig_pair(Mat< std::complex<typename T1::pod_type> >& vals, Mat< std::complex<typename T1::pod_type> >& vecs, const bool vecs_on, const Base< std::complex<typename T1::pod_type>, T1 >& A_expr, const Base< std::complex<typename T1::pod_type>, T2 >& B_expr);
-
-
+  
+  
   //
   // eig_pair_twosided
-
+  
   template<typename T1, typename T2>
   inline static bool eig_pair_twosided(Mat< std::complex<typename T1::pod_type> >& vals, Mat< std::complex<typename T1::pod_type> >& lvecs, Mat< std::complex<typename T1::pod_type> >& rvecs, const Base<typename T1::pod_type,T1>& A_expr, const Base<typename T1::pod_type,T2>& B_expr);
-
+  
   template<typename T1, typename T2>
   inline static bool eig_pair_twosided(Mat< std::complex<typename T1::pod_type> >& vals, Mat< std::complex<typename T1::pod_type> >& lvecs, Mat< std::complex<typename T1::pod_type> >& rvecs, const Base< std::complex<typename T1::pod_type>, T1 >& A_expr, const Base< std::complex<typename T1::pod_type>, T2 >& B_expr);
-
+  
   
   //
   // eig_sym
