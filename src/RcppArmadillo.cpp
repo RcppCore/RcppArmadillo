117f60c1ca0a69f747e567753effb61b544e201a
Dirk Eddelbuettel <edd@debian.org>
2020-10-26 07:26:13 -0500

    version getter is now more robust to armadillo and compilers

--- src/RcppArmadillo.cpp
+++ src/RcppArmadillo.cpp
@@ -32,24 +32,25 @@
 // [[Rcpp::export]]
 Rcpp::IntegerVector armadillo_version(bool single) {
 
-    if (single) {
-        return Rcpp::wrap(10000*arma::arma_version::major +
-                          100*arma::arma_version::minor +
-                          arma::arma_version::patch) ;
-    }
+    // These are declared as constexpr in Armadillo which actually does not define them
+    // They are also defined as macros in arma_version.hpp so we just use that
+    const unsigned int major = ARMA_VERSION_MAJOR;
+    const unsigned int minor = ARMA_VERSION_MINOR;
+    const unsigned int patch = ARMA_VERSION_PATCH;
 
-    Rcpp::IntegerVector version =
-        Rcpp::IntegerVector::create(Rcpp::Named("major") = arma::arma_version::major,
-                                    Rcpp::Named("minor") = arma::arma_version::minor,
-                                    Rcpp::Named("patch") = arma::arma_version::patch);
-    
-    return version ;
+    if (single) {
+        return Rcpp::wrap(10000 * major + 100 * minor + patch) ;
+    } else {
+        return Rcpp::IntegerVector::create(Rcpp::Named("major") = major,
+                                           Rcpp::Named("minor") = minor,
+                                           Rcpp::Named("patch") = patch);
+    }
 }
 
 // Per request of Gábor Csárdi in https://github.com/RcppCore/RcppArmadillo/issues/11
 //
 //' Set the Armadillo Random Number Generator to a random value
-//' 
+//'
 //' @details
 //' Depending on whether RcppArmadillo was compiled for the C++98 standard
 //' (currently the default) or for C++11 (optional), two different RNGs may be used.
@@ -60,32 +61,32 @@ Rcpp::IntegerVector armadillo_version(bool single) {
 //' @return The function is invoked for its side effect and has no return value.
 //' @note This has been found to not work as espected in \pkg{RStudio}
 //' as its code also uses the system RNG library. You may have to either
-//' not run within \pkg{RStudio} or change your code to use a different RNG such 
+//' not run within \pkg{RStudio} or change your code to use a different RNG such
 //' as the one from R.
-//' @seealso The R documentation on its RNGs all of which are accessible via \pkg{Rcpp}.  
+//' @seealso The R documentation on its RNGs all of which are accessible via \pkg{Rcpp}.
 // [[Rcpp::export]]
 void armadillo_set_seed_random() {
     arma::arma_rng::set_seed_random();  			// set the seed to a random value
-} 
+}
 
 //' Set the Armadillo Random Number Generator to the given value
-//' 
+//'
 //' @param val The seed used to initialize Armadillo's random number generator.
-//' @details 
+//' @details
 //' Depending on whether RcppArmadillo was compiled for the C++98 standard
 //' (currently the default) or for C++11 (optional), two different RNGs may be used.
 //' This function resets either. For C++98, the R programming language's RNG is used.
 //' For C++11, the RNG included in the \code{<random>} library is used only when
 //'  \code{#define ARMA_USE_CXX11_RNG} is placed before \code{#include <RcppArmadillo.h>}.
 //'  Otherwise, the R programming language's RNG will be used.
-//' @return The function is invoked for its side effect and has no return value. 
+//' @return The function is invoked for its side effect and has no return value.
 //' @note This has been found to not work as espected in \pkg{RStudio}
 //' as its code also uses the system RNG library. You may have to either
-//' not run within \pkg{RStudio} or change your code to use a different RNG such 
+//' not run within \pkg{RStudio} or change your code to use a different RNG such
 //' as the one from R.
-//' @seealso The R documentation on its RNGs all of which are accessible via \pkg{Rcpp}.  
+//' @seealso The R documentation on its RNGs all of which are accessible via \pkg{Rcpp}.
 // [[Rcpp::export]]
 void armadillo_set_seed(unsigned int val) {
     //Rcpp::Rcout << "Setting value " << val << std::endl;
     arma::arma_rng::set_seed(val);  			    // set the seed to given value
-} 
+}
