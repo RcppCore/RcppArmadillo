25b3e0dafcda58d13a3fe4677cb3bdce524589bf
Dirk Eddelbuettel <edd@debian.org>
2022-02-23 12:53:49 -0600

    updated test wrapper

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,15 +1,4 @@
 
-if (requireNamespace("tinytest", quietly=TRUE) &&
-    utils::packageVersion("tinytest") >= "1.0.0") {
-
-    ## Set a seed to make the test deterministic
-    set.seed(42)
-
-    ## R makes us to this
-    Sys.setenv("R_TESTS"="")
-
-    ## there are several more granular ways to test files in a tinytest directory,
-    ## see its package vignette; tests can also run once the package is installed
-    ## using the same command `test_package(pkgName)`, or by director or file
-    tinytest::test_package("RcppArmadillo", ncpu=getOption("Ncpus", 1))
+if (requireNamespace("tinytest", quietly=TRUE)) {
+    tinytest::test_package("RcppArmadillo")
 }
