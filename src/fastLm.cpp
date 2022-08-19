97264622b17d7be11368143792b043daba0b879f
Dirk Eddelbuettel <edd@debian.org>
2022-08-18 19:19:20 -0500

    simplification for fastLm() in example and README.md
    with special thanks to @conradsnicta for pointing this out

--- src/fastLm.cpp
+++ src/fastLm.cpp
@@ -2,7 +2,7 @@
 //
 // fastLm.cpp: Rcpp/Armadillo glue example of a simple lm() alternative
 //
-// Copyright (C)  2010 - 2017  Dirk Eddelbuettel, Romain Francois and Douglas Bates
+// Copyright (C)  2010 - 2022  Dirk Eddelbuettel, Romain Francois and Douglas Bates
 //
 // This file is part of RcppArmadillo.
 //
@@ -25,17 +25,16 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 List fastLm_impl(const arma::mat& X, const arma::colvec& y) {
     int n = X.n_rows, k = X.n_cols;
-        
+
     arma::colvec coef = arma::solve(X, y);    // fit model y ~ X
     arma::colvec res  = y - X*coef;           // residuals
 
     // std.errors of coefficients
-    double s2 = std::inner_product(res.begin(), res.end(), res.begin(), 0.0)/(n - k);
-                                                        
-    arma::colvec std_err = arma::sqrt(s2 * arma::diagvec(arma::pinv(arma::trans(X)*X)));  
+    double s2 = arma::dot(res, res) / (n - k);
+
+    arma::colvec std_err = arma::sqrt(s2 * arma::diagvec(arma::pinv(arma::trans(X)*X)));
 
     return List::create(Named("coefficients") = coef,
                         Named("stderr")       = std_err,
                         Named("df.residual")  = n - k);
 }
-
