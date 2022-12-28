ade659fcff9e01d6add2e2e6ebf5e8fa7768e0b6
Dirk Eddelbuettel <edd@debian.org>
2022-12-28 12:08:11 -0600

    Armadillo 11.4.3

--- inst/include/armadillo_bits/arma_config.hpp
+++ inst/include/armadillo_bits/arma_config.hpp
@@ -174,6 +174,13 @@ struct arma_config
   #endif
   
   
+  #if defined(ARMA_HAVE_CXX20)
+    static constexpr bool cxx20 = true;
+  #else
+    static constexpr bool cxx20 = false;
+  #endif
+  
+  
   #if (!defined(ARMA_DONT_USE_STD_MUTEX))
     static constexpr bool std_mutex = true;
   #else
